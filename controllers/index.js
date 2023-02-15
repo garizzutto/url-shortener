49e0bfb463c8de3bbf0c666e3e399b626b49ee69
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2023-02-15 17:58:04 -0300

    feat: adding try-catch block on controller

--- controllers/index.js
+++ controllers/index.js
@@ -7,43 +7,53 @@ function generateShortUrl() {
 }
 
 export async function getShortUrl(req, res) {
-  const longUrl = req.body.longUrl;
-  if(longUrl){
-    let shortUrl = generateShortUrl();
-    let isUrlUnique = false;
-    for(let i = 0; i < 5 && !isUrlUnique; i++){
-      const urlMap = await UrlMap.findOne({shortUrl})
-      if(!urlMap){
-        isUrlUnique = true;
+  try{
+    const longUrl = req.body.longUrl;
+    if(longUrl){
+      let shortUrl = generateShortUrl();
+      let isUrlUnique = false;
+      for(let i = 0; i < 5 && !isUrlUnique; i++){
+        const urlMap = await UrlMap.findOne({shortUrl})
+        if(!urlMap){
+          isUrlUnique = true;
+        }
+        else{
+          shortUrl = generateShortUrl();
+        }
       }
-      else{
-        shortUrl = generateShortUrl();
+      if(!isUrlUnique){
+        res.status(500).send({error: 'Could not generate unique short url, try again later'});
+        return;
       }
+      const urlMap = await UrlMap.create({longUrl, shortUrl});
+      await urlMap.save();
+      res.send({shortUrl, shortUrlExpiresAt: new Date(Date.now() + Number(process.env.EXPIRE_TIME))});
     }
-    if(!isUrlUnique){
-      res.status(500).send({error: 'Could not generate unique short url, try again later'});
-      return;
+    else{
+      res.status(400).send({error: 'No long url provided'});
     }
-    const urlMap = await UrlMap.create({longUrl, shortUrl});
-    await urlMap.save();
-    res.send({shortUrl, shortUrlExpiresAt: new Date(Date.now() + Number(process.env.EXPIRE_TIME))});
-  }
-  else{
-    res.status(400).send({error: 'No long url provided'});
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({error: 'Could not generate short url, try again later'});
   }
 }
 
 export async function getLongUrl(req, res) {
-  const shortUrl = req.body.shortUrl;
-  if(shortUrl){
-    const urlMap = await UrlMap.findOne({shortUrl, created_at: {$gt: new Date(Date.now() - Number(process.env.EXPIRE_TIME))}});
-    if(!urlMap){
-      res.status(404).send({error: 'No long url found for this short url'});
-      return;
+  try{
+    const shortUrl = req.body.shortUrl;
+    if(shortUrl){
+      const urlMap = await UrlMap.findOne({shortUrl, created_at: {$gt: new Date(Date.now() - Number(process.env.EXPIRE_TIME))}});
+      if(!urlMap){
+        res.status(404).send({error: 'No long url found for this short url'});
+        return;
+      }
+      res.send({longUrl: urlMap.longUrl, shortUrlExpiresAt: new Date(urlMap.created_at + Number(process.env.EXPIRE_TIME))});
     }
-    res.send({longUrl: urlMap.longUrl, shortUrlExpiresAt: new Date(urlMap.created_at + Number(process.env.EXPIRE_TIME))});
-  }
-  else{
-    res.status(400).send({error: 'No short url provided'});
+    else{
+      res.status(400).send({error: 'No short url provided'});
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({error: 'Could not get long url, try again later'});
   }
 }
